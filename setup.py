a068d1116fca35edf615d5eb3169a93891aca363
Darius Aliulis <daraliu@ktu.edu>
2015-09-15 05:19:40 +0300

    setup details

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-    name='bdmm_test_helper',
-    packages=['bdmm_test_helper'],
-    version='0.2',
+    name='test_helper',
+    packages=['test_helper'],
+    version='0.3',
     description='github.com/hpec/test_helper for Python 3',
     author='',
     author_email='',
     url='https://github.com/daraliu/test_helper',
-    download_url='https://github.com/daraliu/test_helper/tarball/0.1',
+    download_url='https://github.com/daraliu/test_helper/tarball/0.3',
     keywords=['testing'],
     classifiers=[],
 )
