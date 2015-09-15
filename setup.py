bd4762de79f1bbbb4dc2b6155b7f21f383325300
Darius Aliulis <daraliu@ktu.edu>
2015-09-15 05:07:57 +0300

    adapted for python 3

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'test_helper',
-  packages = ['test_helper'],
-  version = '0.2',
-  description = 'A testing helper for scalable machine learning mooc',
-  author = 'Daniel Liu',
-  author_email = 'hpec.liu@gmail.com',
-  url = 'https://github.com/hpec/test_helper',
-  download_url = 'https://github.com/hpec/test_helper/tarball/0.1',
-  keywords = ['testing', 'autograder', 'mooc'],
-  classifiers = [],
+    name='bdmm_test_helper',
+    packages=['bdmm_test_helper'],
+    version='0.2',
+    description='github.com/hpec/test_helper for Python 3',
+    author='',
+    author_email='',
+    url='https://github.com/daraliu/test_helper',
+    download_url='https://github.com/daraliu/test_helper/tarball/0.1',
+    keywords=['testing'],
+    classifiers=[],
 )
--- test_helper/test_helper.py
+++ test_helper/test_helper.py
@@ -1,50 +1,55 @@
+from __future__ import print_function
 import hashlib
 
-class TestFailure(Exception):
-  pass
-class PrivateTestFailure(Exception):
-  pass
 
-class Test(object):
-  passed = 0
-  numTests = 0
-  failFast = False
-  private = False
+class testfailure(Exception):
+    pass
 
-  @classmethod
-  def setFailFast(cls):
-    cls.failFast = True
 
-  @classmethod
-  def setPrivateMode(cls):
-    cls.private = True
+class privatetestfailure(Exception):
+    pass
 
-  @classmethod
-  def assertTrue(cls, result, msg=""):
-    cls.numTests += 1
-    if result == True:
-      cls.passed += 1
-      print "1 test passed."
-    else:
-      print "1 test failed. " + msg
-      if cls.failFast:
-        if cls.private:
-          raise PrivateTestFailure(msg)
+
+class test(object):
+    passed = 0
+    numtests = 0
+    failfast = False
+    private = False
+
+    @classmethod
+    def setfailfast(cls):
+        cls.failfast = True
+
+    @classmethod
+    def setprivatemode(cls):
+        cls.private = True
+
+    @classmethod
+    def asserttrue(cls, result, msg=""):
+        cls.numtests += 1
+        if result is True:
+            cls.passed += 1
+            print("1 test passed.")
         else:
-          raise TestFailure(msg)
+            print("1 test failed. {}".format(msg))
+            if cls.failfast:
+                if cls.private:
+                    raise privatetestfailure(msg)
+                else:
+                    raise testfailure(msg)
 
-  @classmethod
-  def assertEquals(cls, var, val, msg=""):
-    cls.assertTrue(var == val, msg)
+    @classmethod
+    def assertequals(cls, var, val, msg=""):
+        cls.asserttrue(var == val, msg)
 
-  @classmethod
-  def assertEqualsHashed(cls, var, hashed_val, msg=""):
-    cls.assertEquals(cls._hash(var), hashed_val, msg)
+    @classmethod
+    def assertequalshashed(cls, var, hashed_val, msg=""):
+        cls.assertequals(cls._hash(var), hashed_val, msg)
 
-  @classmethod
-  def printStats(cls):
-    print "{0} / {1} test(s) passed.".format(cls.passed, cls.numTests)
+    @classmethod
+    def printstats(cls):
+        print("{0} / {1} test(s) passed.".format(cls.passed, cls.numtests))
 
-  @classmethod
-  def _hash(cls, x):
-    return hashlib.sha1(str(x)).hexdigest()
+    @classmethod
+    def _hash(cls, x):
+        return hashlib.sha1(str(x).encode("utf-8")).hexdigest()
